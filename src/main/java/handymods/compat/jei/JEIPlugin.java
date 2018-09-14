39e57b66c5a3d77c66bfa968f18fd87e59675c20
juliand665 <julian.dunskus@gmail.com>
2018-09-14 04:19:49 +0200

    added JEI item moving support

---
+++ src/main/java/handymods/compat/jei/JEIPlugin.java
@@ -0,0 +1,19 @@
+package handymods.compat.jei;
+
+import handymods.inventory.ContainerChestyCraftingTable;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
+
+@mezz.jei.api.JEIPlugin
+public class JEIPlugin implements IModPlugin {
+	@Override
+	public void register(IModRegistry registry) {
+		registry.getRecipeTransferRegistry().addRecipeTransferHandler(
+				ContainerChestyCraftingTable.class,
+				VanillaRecipeCategoryUid.CRAFTING,
+				1, 9,
+				10, 36
+		);
+	}
+}

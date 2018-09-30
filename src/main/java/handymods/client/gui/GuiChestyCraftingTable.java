97cb9a9aba12cebad2f7ea311d9e34b94324445b
juliand665 <julian.dunskus@gmail.com>
2018-09-30 21:24:42 +0200

    fixed tooltips missing from CCT gui

--- src/main/java/handymods/client/gui/GuiChestyCraftingTable.java
+++ src/main/java/handymods/client/gui/GuiChestyCraftingTable.java
@@ -18,6 +18,13 @@ public class GuiChestyCraftingTable extends GuiContainer {
 	}
 	
 	@Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+		this.drawDefaultBackground();
+		super.drawScreen(mouseX, mouseY, partialTicks);
+		this.renderHoveredToolTip(mouseX, mouseY);
+	}
+	
+	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
 		fontRenderer.drawString(I18n.format("container.crafting"), 28, 6, 0x404040);
 		fontRenderer.drawString(I18n.format("container.inventory"), 8, ySize - 96 + 2, 0x404040);
@@ -25,7 +32,6 @@ public class GuiChestyCraftingTable extends GuiContainer {
 	
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-		drawDefaultBackground();
 		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 		mc.getTextureManager().bindTexture(backgroundTexture);
 		drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);

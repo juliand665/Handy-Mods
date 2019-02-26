2be480126b1fb7ba99584fd08050a3b560ba01f2
Julian Dunskus <julian.dunskus@gmail.com>
2019-02-26 03:18:15 +0100

    improved localization api

--- src/main/java/handymods/util/Localization.java
+++ src/main/java/handymods/util/Localization.java
@@ -1,17 +1,23 @@
 package handymods.util;
 
+import handymods.HandyMods;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public class Localization {
-	/** e.g. keyString("tooltip", enderBox, "empty") -> "tooltip.handymods.ender_box.empty") */
-	public static String keyString(String domain, IForgeRegistryEntry entry, String path) {
-		final ResourceLocation location = entry.getRegistryName();
-		return domain + "." + location.getResourceDomain() + "." + location.getResourcePath() + "." + path;
+	/** e.g. keyString("tooltip", "ender_box.empty") -> "tooltip.handymods.ender_box.empty") */
+	public static String keyString(String domain, String path) {
+		return domain + "." + HandyMods.MOD_ID + "." + path;
 	}
 	
 	public static String localized(String domain, IForgeRegistryEntry entry, String path, Object... params) {
-		return I18n.format(keyString(domain, entry, path), params);
+		final ResourceLocation location = entry.getRegistryName();
+		assert location != null;
+		return localized(domain, location.getResourcePath() + "." + path, params);
+	}
+	
+	public static String localized(String domain, String path, Object... params) {
+		return I18n.format(keyString(domain, path), params);
 	}
 }
